3a51bae7d7c13482ca3c16996d5f5f5be0c0c405
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2024-09-08 10:04:58 +0200

    Agregar gitignore y carpeta funciones con el archivo built_in.py

---
+++ funciones/built_in.py
@@ -0,0 +1,15 @@
+numeros = [8,7,3,98,67,45,13,105,54,78]
+
+#Buscar el número más alto de una lista
+numero_mas_alto = max(numeros)
+print(f'El número más alto de la lista es : {numero_mas_alto}') #105
+
+#Buscar el número más bajo de una lista
+numero_mas_bajo = min(numeros)
+print('El número más bajo de la lista es : {}'.format(numero_mas_bajo)) #3
+print('El número más bajo es {} y el más alto es {}'.format(numero_mas_bajo, numero_mas_alto))
+
+#Redondear un número con x decimales dados por parámetro
+numero = 18.698536
+print(round(numero,3)) #18.698
+
